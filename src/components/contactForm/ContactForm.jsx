2c1f48115da2b778deb5338aebb875b5004c7828
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-05-11 21:40:41 +0300

    add

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -1,14 +1,18 @@
 //import css from "./ContactForm.module.css";
-import { nanoid } from "nanoid";
+//import React from "react";
+import useId from "react";
+//import { nanoid } from "nanoid";
 import { Formik, Form, Field } from "formik";
 //import { number } from "prop-types";
 export default function ContactForm({ onAdd }) {
+  const fieldId = useId();
   const handleSubmit = (values, actions) => {
     onAdd(values);
     actions.resetForm();
   };
   //model.id = nanoid();
   // onAdd({
+  // id: Date.now();
   //   name: e.target.elements.name.values,
   //   number: e.target.elements.number.values,
   // });
@@ -16,12 +20,12 @@ export default function ContactForm({ onAdd }) {
     <Formik initialValues={{ name: "", number: "" }} onSubmit={handleSubmit}>
       <Form>
         <div>
-          <label>Name</label>
-          <Field type="text" name="username" />
+          <label htmlFor={`${fieldId}-username`}>Name</label>
+          <Field type="text" name="username" id={`${fieldId}-username`} />
         </div>
         <div>
-          <label>Number</label>
-          <Field type="text" name="number" />
+          <label htmlFor={`${fieldId}-number`}>Number</label>
+          <Field type="text" name="number" id={`${fieldId}-number`} />
         </div>
 
         <button type="submit">Add contact</button>
