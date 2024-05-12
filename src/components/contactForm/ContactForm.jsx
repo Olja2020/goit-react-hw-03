b33b913f56f319951974d893a197c3e3808c832f
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-05-12 10:11:38 +0300

    add

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -1,26 +1,15 @@
 import css from "./ContactForm.module.css";
 import { useId } from "react";
-//import React from "react";
-
 import * as Yup from "yup";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-// const SignupSchema = Yup.object().shape({
-//   firstName: Yup.string()
-//     .min(2, "Too Short!")
-//     .max(50, "Too Long!")
-//     .required("Required"),
-//   lastName: Yup.string()
-//     .min(2, "Too Short!")
-//     .max(50, "Too Long!")
-//     .required("Required"),
-//   email: Yup.string().email("Invalid email").required("Required"),
-// });
+
 const UserSchema = Yup.object().shape({
   name: Yup.string()
     .min(3, "min 3 letter")
     .max(50, "max 50 letter")
     .required("required field"),
-  number: Yup.number()
+  number: Yup.string()
+
     .min(3, "min 3 number")
     .max(50, "max 50 number")
     .required("required field"),
